c9f31c53d8569c534acc8aacc762e9f4439c96b6
tcgontijo <tulio_gontijo@hotmail.com>
2021-03-27 22:43:26 -0300

    Commit inicial

---
+++ ajax/aplicacao02/js/functions.js
@@ -0,0 +1,32 @@
+function loadCustomers(idTable) {
+    let xhttp = new XMLHttpRequest();
+    let file = "https://github.com/tcgontijo/PFJS/blob/master/json/clientes.json";
+
+    xhttp.onreadystatechange = () => {
+        if ((xhttp.readyState == 4) && (xhttp.status == 200)) {
+            printCustomers(xhttp.responseText, idTable);
+        }
+    }
+
+    xhttp.open("GET", file, true);
+    xhttp.send();
+
+}
+
+function printCustomers(clientes, idTable) {
+    var clientes = JSON.parse();
+
+    var tabela = document.getElementById(idTable);
+
+    clientes.forEach(cliente => {
+        let linha = document.createElement("tr");
+        let tdNome = document.createElement("td");
+        let tdIdade = document.createElement("td");
+        tdNome.textContent = cliente.nome;
+        tdIdade.textContent = cliente.idade;
+        linha.appendChild(tdNome);
+        linha.appendChild(tdIdade);
+        tabela.appendChild(linha);
+    }
+
+}
