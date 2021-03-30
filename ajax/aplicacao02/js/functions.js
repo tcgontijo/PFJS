8dc3c710faf8823163da671a04610e817bbce27d
tcgontijo <tulio_gontijo@hotmail.com>
2021-03-30 14:19:09 -0300

    criacao da tabela

--- ajax/aplicacao02/js/functions.js
+++ ajax/aplicacao02/js/functions.js
@@ -1,12 +1,8 @@
 function loadCustomers(idTable) {
     let xhttp = new XMLHttpRequest();
-<<<<<<< HEAD
     //let file = "https://tcgontijo.github.io/PFJS/ajax/aplicacao02/json/clientes.json";
-    let file = "../json/clientes.json"
-=======
-    let file = "https://tcgontijo.github.io/PFJS/ajax/aplicacao02/json/clientes.json";
+    let file = "json/clientes.json"
 
->>>>>>> 681e6c8b44c8c7498baa405dc6191ae491e05cfd
     xhttp.onreadystatechange = () => {
         if ((xhttp.readyState == 4) && (xhttp.status == 200)) {
             printCustomers(xhttp.responseText, idTable);
@@ -20,12 +16,11 @@ function loadCustomers(idTable) {
 
 function printCustomers(clientes, idTable) {
     console.log(clientes);
-    var clientes = JSON.parse();
-    console.log(clientes);
+    var Listaclientes = JSON.parse(clientes);
 
     var tabela = document.getElementById(idTable);
 
-    clientes.clientes.forEach(cliente => {
+    Listaclientes.clientes.forEach(cliente => {
         let linha = document.createElement("tr");
         let tdNome = document.createElement("td");
         let tdIdade = document.createElement("td");
